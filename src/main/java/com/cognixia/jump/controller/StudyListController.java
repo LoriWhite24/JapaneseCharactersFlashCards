bc50eab2aa0c71a3fad810b1cc60f300c13f4fc5
Lori White <lori.r.white.24@gmail.com>
2020-08-10 03:33:01 -0400

    Tested the initial page of the Front End

--- src/main/java/com/cognixia/jump/controller/StudyListController.java
+++ src/main/java/com/cognixia/jump/controller/StudyListController.java
@@ -24,20 +24,6 @@ public class StudyListController {
 	@Autowired
 	StudyListRepository service;
 	
-	@GetMapping("/study")
-	public String getMain() {
-		return "<!DOCTYPE html>\r\n" + 
-				"<html>\r\n" + 
-				"<body>\r\n" + 
-				"\r\n" + 
-				"<h1>自習時間</h1>\r\n" + 
-				"\r\n" + 
-				"<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/3002_Kanji.svg/957px-3002_Kanji.svg.png\" alt=\"Study Hard!\">\r\n" + 
-				"\r\n" + 
-				"</body>\r\n" + 
-				"</html>";
-	}
-	
 	@GetMapping("/studylists")
 	public List<StudyList> getAllStudyLists() {
 		return service.findAll();
--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,1 +1,18 @@
-// JavaScript source code
+﻿// JavaScript source code
+function initialize() {
+    document.getElementById("pagetitle").insertAdjacentHTML('beforeend', "<h1>自習時間</h1>");
+    document.getElementById("pagebody").insertAdjacentHTML('beforeend', "<div class=\"text-center\"><img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/3002_Kanji.svg/957px-3002_Kanji.svg.png\" alt=\"Study Hard!\"></div>");
+    //getPage("/api/study");
+}
+
+/*function getPage(url) {
+    var xhttpList = new XMLHttpRequest();
+    xhttpList.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            renderRequest(this.responseText);
+        }
+    };
+    xhttpList.open("GET", url, true);
+    xhttpList.send();
+    console.log("Page received.");
+}*/
